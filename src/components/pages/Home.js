13e1a3e8901ba0e3877660ac8da2819ae34de2d5
Liam5305 <liamrutherford10@gmail.com>
2022-01-21 17:27:09 +0000

    Updated JSON and fixed items appearing

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,52 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
 import bagImg from "../../imgs/backpack1.png"
 import laptopImg from "../../imgs/laptop1.png";
 import notepadImg from "../../imgs/notepad1.jpg";
 import Button from "@mui/material/Button";
 import { shadows } from '@mui/system';
 import Box from '@mui/material/Box';
-import auctionItems from "../AuctionItems";
+import auctionItemsData from "../auction-items.json";
 
 function HomePage() {
+    const [item, setItems] = useState("")
+
     return (
         <>
-        <div>
-            <div className="seasonal-advert">
-                <Box sx={{
-                    boxShadow: 3,
-                    padding: 2,
-                    backgroundColor: "#4361FF"
-                }} >
-                    <h2>Back to Uni?</h2>
-                    <h3>We've got all that you need!</h3>
+            <div>
+                <div className="seasonal-advert">
+                    <Box sx={{
+                        boxShadow: 3,
+                        padding: 2,
+                        backgroundColor: "#4361FF"
+                    }} >
+                        <h2>Back to Uni?</h2>
+                        <h3>We've got all that you need!</h3>
 
-                    {/* Laptop Image */}
-                    <img src={laptopImg} alt="Laptop" width="200" height="250" ></img>
-                    {/* NotePad Image */}
-                    <img src={notepadImg} alt="Notepad" width="200" height="250" ></img>
-                    {/* BackPack Image */}
-                    <img src={bagImg} alt="BackPack" width="200" height="250" ></img>
-                    <br />
-                    <Button
-                        style={{
-                            borderColor: "#f8f8f8",
-                            color: "#f8f8f8"
-                        }}
-                        variant="outlined">Shop Now!
-                    </Button>
-                </Box>
+                        {/* Laptop Image */}
+                        <img src={laptopImg} alt="Laptop" width="200" height="250" ></img>
+                        {/* NotePad Image */}
+                        <img src={notepadImg} alt="Notepad" width="200" height="250" ></img>
+                        {/* BackPack Image */}
+                        <img src={bagImg} alt="BackPack" width="200" height="250" ></img>
+                        <br />
+                        <Button
+                            style={{
+                                borderColor: "#f8f8f8",
+                                color: "#f8f8f8"
+                            }}
+                            variant="outlined">Shop Now!
+                        </Button>
+                    </Box>
+                </div>
+                <br />
+
+                <div>
+
+                    {
+                        auctionItemsData.filter(post => {
+                            if (item === '') {
+                                return post;
+                            } else if (post.item.toLowerCase().includes(item.toLowerCase())) {
+                                return post;
+                            }
+                        }).map((post, index) => (
+                            <div className="auction-items" key={index}>
+                                <h3>{post.item}</h3>
+                                <p>Current Bid:</p>
+                                <div id="post-price">
+                                    <p>£{post.price}</p>
+                                </div>
+                                <button>Select</button>
+                            </div>
+                        ))
+                    }
+                </div>
             </div>
-            <br />
-                    
-            {/* <div>
-                {auctionItems.map((data) => (
-                    <li key={data.id}>
-                        <p>{data.name}</p>
-                        <p>{data.price}</p>
-                    </li>
-                ))}
-            </div> */}
-        </div>
         </>
     );
 }
